f9497a6dd5c5398421e7f37eee443f1213b37daa
xxh3898 <chiho3898@gmail.com>
2025-12-15 11:38:23 +0900

    fix: BaseTimeEntity Auditing 설정 오류 수정
    - @EntityListeners에 잘못 지정된 에러 클래스 수정 (AuditingEntityListener 적용)
    - 수정 시간 필드 어노테이션 변경 (@LastModifiedBy -> @LastModifiedDate)

--- backend/src/main/java/com/cube/cube_server/domain/BaseTimeEntity.java
+++ backend/src/main/java/com/cube/cube_server/domain/BaseTimeEntity.java
@@ -5,12 +5,13 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
-@EntityListeners(AbstractMethodError.class)
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class BaseTimeEntity {
 
@@ -18,6 +19,6 @@ public abstract class BaseTimeEntity {
     @Column(updatable = false)
     private LocalDateTime createTime;
 
-    @LastModifiedBy
+    @LastModifiedDate
     private LocalDateTime updateTime;
 }
