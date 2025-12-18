c3e1b20857ee74780274385d293b8295dec4b8ff
xxh3898 <chiho3898@gmail.com>
2025-12-18 17:32:05 +0900

    refactor(frontend): 불필요한 코드 및 useBoardStore.js 삭제

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import { useEffect } from 'react'
 import AppRoutes from './routes/routes'
 import useMemberStore from './stores/useMemberStore'
-import useBoardStore from './stores/useBoardStore'
 
 function App() {
   const { initialize } = useMemberStore();
-  const { initializeBoard } = useBoardStore();
 
   useEffect(() => {
-    initialize(); 
-    initializeBoard();
+    initialize();
   }, [])
 
   return (
