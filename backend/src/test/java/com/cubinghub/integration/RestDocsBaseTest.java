100cfd7641758dcc93f6789f40b4bcf36e96137b
xxh3898 <chiho3898@gmail.com>
2026-03-25 15:13:33 +0900

    feat: Rest Docs 설정 클래스 및 MockMvc 기반 추상 테스트 클래스 추가
    - RestDocsConfig: prettyPrint 전처리기 적용으로 스니펫 가독성 향상
    - RestDocsBaseTest: BaseIntegrationTest 상속, RestDocumentationExtension 연동
    - index.adoc: 헬스체크 API 스니펫 include 템플릿 초안 작성

---
+++ backend/src/test/java/com/cubinghub/integration/RestDocsBaseTest.java
@@ -0,0 +1,28 @@
+package com.cubinghub.integration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.cubinghub.config.RestDocsConfig;
+
+@ExtendWith(RestDocumentationExtension.class)
+@Import(RestDocsConfig.class)
+public abstract class RestDocsBaseTest extends BaseIntegrationTest {
+
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUpMockMvc(WebApplicationContext webApplicationContext,
+                      RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+}
