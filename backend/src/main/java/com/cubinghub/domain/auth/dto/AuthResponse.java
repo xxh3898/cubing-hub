e4de1946f67d7ae1eb50d5e1b4fc696ea273047a
xxh3898 <chiho3898@gmail.com>
2026-03-30 11:59:34 +0900

    feat: auth 도메인용 공통 DTO 추가

---
+++ backend/src/main/java/com/cubinghub/domain/auth/dto/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.cubinghub.domain.auth.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class AuthResponse {
+
+    private final String accessToken;
+    private final String tokenType = "Bearer";
+
+    @Builder
+    public AuthResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
